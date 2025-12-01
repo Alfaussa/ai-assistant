1e8dda1a20514a02cf2061424d0e666ca8f5d310
Alfiya <al.yausheva@gmail.com>
2025-12-01 23:18:35 +0300

    formatting styles in page.js

--- app/page.js
+++ app/page.js
@@ -1,85 +1,84 @@
 
 import BoilerCalculator from '../components/BoilerCalculator.js';
 import Chat from '../components/Chat.jsx';
-import {Calculator, MessageCircle, Flame, BarChart3, Info, Heater } from "lucide-react";
-  
+import { Calculator, MessageCircle, Flame, BarChart3, Info, Heater } from "lucide-react";
+
 
 export const metadata = {
   title: 'My page title',
   description: 'My page description',
 }
-export default function Home({history, setHistory}) {
+export default function Home({ history, setHistory }) {
 
-  
-  
-  
-  
-  
+
   return (
 
-      <div style={{ maxWidth: "1000px", margin: "0 auto", padding: "20px",
-        backgroundColor: '#D9D7DD' 
+    <div style={{
+      maxWidth: "1000px", margin: "0 auto", padding: "20px",
+      backgroundColor: '#D9D7DD'
 
-    
+
+    }}>
+
+      <header style={{
+        position: "fixed", top: 0, left: 0, width: "100%",
+        background: "rgba(255, 255, 255, 0.9)",
+        backdropFilter: "blur(8px)",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "8px 16px",
+        boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
+        flexWrap: "wrap",
+        zIndex: 1000,
       }}>
-    
-      <header style={{position: "fixed", top: 0, left: 0,width: "100%",
-    background: "rgba(255, 255, 255, 0.9)",
-    backdropFilter: "blur(8px)",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "8px 16px",
-    boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
-      flexWrap: "wrap", 
-    zIndex: 1000, 
-  }}>
-        <div style={{display:'flex'
-          
+        <div style={{
+          display: 'flex'
+
         }}>
-        <Heater size={36} style={{paddingTop:'4px', marginRight:'5px',  minWidth: "36px",minHeight: "36px", marginRight: "8px",}} /> 
-        <h1 style={{marginBottom:'10px'}}>Smart Boiler Selector</h1></div>
-        <nav style={{ display: 'flex', gap: '20px', fontSize: '18px', display: "flex",flexWrap: "wrap", justifyContent: "center",marginRight: "30px"}}>
-      <a href="#chat" style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-        <MessageCircle size={20} /> Chat
-      </a>
-      <a href="#calc" style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-        <Flame size={20} /> Calculator
-      </a>
-      <a href="#chart" style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-        <BarChart3 size={20} /> Graph
-      </a>
-      <a href="#about" style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-        <Info size={20} /> About
-      </a>
-    </nav>
+          <Heater size={36} style={{ paddingTop: '4px', marginRight: '5px', minWidth: "36px", minHeight: "36px", marginRight: "8px", }} />
+          <h1 style={{ marginBottom: '10px' }}>Smart Boiler Selector</h1></div>
+        <nav style={{ display: 'flex', gap: '20px', fontSize: '18px', display: "flex", flexWrap: "wrap", justifyContent: "center", marginRight: "30px" }}>
+          <a href="#chat" >
+            <MessageCircle size={20} /> Chat
+          </a>
+          <a href="#calc" >
+            <Flame size={20} /> Calculator
+          </a>
+          <a href="#chart" >
+            <BarChart3 size={20} /> Graph
+          </a>
+          <a href="#about" >
+            <Info size={20} /> About
+          </a>
+        </nav>
       </header>
 
       <main>
-        <section id="chat" style={{ marginBottom: "40px"}}>
-          <h2><MessageCircle size={20} /> Chat</h2>
-         <Chat/>
-        </section>
-        <section id="calc" style={{ marginBottom: "40px" }}>
-          <h2  style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-            <Calculator />Boiler power calculator</h2>
-     
 
-          <BoilerCalculator/>
-   
+        <section id="chat">
+          <h2><MessageCircle size={20} /> Chat</h2>
+          <Chat />
+        </section>
+        <section id="calc" >
+          <h2 style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <Calculator />Boiler power calculator</h2>
+
+
+          <BoilerCalculator />
+
         </section>
         <section id="about">
           <h2><Info size={20} /> About</h2>
           <p>This project helps calculate heating and ask your questions.
-           Fugiat exercitation amet ullamco anim magna velit ut veniam consequat consequat commodo reprehenderit ut. Tempor culpa occaecat aliquip sunt do duis deserunt ut consectetur in ad amet elit quis. Nostrud cupidatat aliqua quis elit magna officia dolore magna ea. Dolor ut elit dolor voluptate aliqua veniam exercitation velit elit dolore.
+            Fugiat exercitation amet ullamco anim magna velit ut veniam consequat consequat commodo reprehenderit ut. Tempor culpa occaecat aliquip sunt do duis deserunt ut consectetur in ad amet elit quis. Nostrud cupidatat aliqua quis elit magna officia dolore magna ea. Dolor ut elit dolor voluptate aliqua veniam exercitation velit elit dolore.
           </p>
-          
         </section>
       </main>
 
-  <footer style={{ marginTop: "40px", borderTop: "1px solid #ccc", paddingTop: "10px" }}>
+      <footer style={{ marginTop: "40px", borderTop: "1px solid #ccc", paddingTop: "10px" }}>
         <p>© 2025 AI for construction | Contacts: info@example.com</p>
       </footer>
-      </div>
-    
+    </div>
+
   )
 }
